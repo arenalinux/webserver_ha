2a48421b58ef7918b78df16b34ffaba01305d981
AFernandes3 <allan.fernandes@ceptis.com.br>
2021-10-19 06:20:20 -0300

    Update Security Groups

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "jenkins" {
   ami = "ami-0ab4d1e9cf9a1215a"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.allow_tls.id}"]
+  security_groups = ["sg_WebServer"]
   tags = {
     Name = "jenkins-server"
   }
@@ -19,64 +19,5 @@ resource "aws_vpc" "main" {
 
   tags = {
     Name = "main"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.main.id}"
-
-  ingress {
-      description      = "TLS from VPC"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-    }
-
-    ingress {
-      description      = "http from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-    }
-
-    ingress {
-      description      = "https from VPC"
-      from_port        = 8080
-      to_port          = 8080
-      protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-    }
-
-    ingress {
-      description      = "ssh from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-    }
-
-    ingress {
-      description      = "ICMP from VPC"
-      from_port        = -1
-      to_port          = -1
-      protocol         = "icmp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-    }
-  
-
-  egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-
-  tags = {
-    Name = "allow_tls_ssh_http(s)"
   }
 }
